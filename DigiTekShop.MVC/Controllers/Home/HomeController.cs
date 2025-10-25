8fce8327a192913f497f9b682d8ade64335ccf1f
elham722 <elham.heydari722@gmail.com>
2025-10-25 19:12:25 +0330

    add template

--- DigiTekShop.MVC/Controllers/Home/HomeController.cs
+++ DigiTekShop.MVC/Controllers/Home/HomeController.cs
@@ -13,14 +13,4 @@ namespace DigiTekShop.MVC.Controllers.Home;
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
