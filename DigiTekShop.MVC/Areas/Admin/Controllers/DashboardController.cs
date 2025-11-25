ddf2a4c130e019bdd7235803f1909f1223ed6c3b
elham <elham@ELHAM>
2025-11-25 15:02:15 +0330

    addd panel admin area

---
+++ DigiTekShop.MVC/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿namespace DigiTekShop.MVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/[controller]/[action]")]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
